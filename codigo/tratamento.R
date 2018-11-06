ea97d48451d0dd6bb42ad4b947bb962df4c9fe9e
Pedro Barros <pedrohnbarros@gmail.com>
2018-11-06 21:38:23 -0200

    adicionando grafico seção 3

--- codigo/tratamento.R
+++ codigo/tratamento.R
@@ -34,11 +34,22 @@ dados2
 dado<- table(dados$idade)
 dado
 
-png(filename="gráficos/faixa_etaria.png", width = 800, height = 500, pointsize = 16)
+png(filename="gráficos/faixa_etaria_pie.png", width = 800, height = 600, pointsize = 20)
 
 par(bg = "light blue") #cor do fundo azul claro
 nomes <- c("16 e 20","21 e 25","26 e 30")
 barplot(dado,names.arg = nomes,ylim=c(0,40),xlab = "Idade",ylab = "Frequência",col = rainbow(3),main = "Faixa etária dos questionados")
 dev.off()
 
+#fazendo grafico principais motivos
+dados <- read_excel("dados/umses_alunos_2018.xlsx")
+dados <- as.data.frame(dados)
 
+d<- c(sum(dados$contato),sum(dados$atualizado),sum(dados$preencher),sum(dados$encontrar),sum(dados$compopiniao),sum(dados$compfoto),sum(dados$amigosja),sum(dados$profnetwork),sum(dados$novaamizade),sum(dados$compdetalhe))
+d
+names(d) <- c("Contato","Atualizar","Tempo livre","Conteudo","Opiniões","Fotos ou vídeos","Amigos","Networking","Pessoas","Assuntos")
+png(filename="gráficos/Seção3_barplot.png", width = 800, height = 600, pointsize = 20)
+
+par(mar = c(7,4,4,4))
+barplot(d,col=rainbow(11),las=2,ylim = c(0,60),main="Principais motivos de uso")
+dev.off()
