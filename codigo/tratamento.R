d80ce60f9cf464ad8e58c6e7ae45667d0fce2254
Pedro Barros <pedrohnbarros@gmail.com>
2018-11-06 20:14:24 -0200

    adicionando a pasta gráfico com o gráfico da faixa etária

--- codigo/tratamento.R
+++ codigo/tratamento.R
@@ -1,5 +1,7 @@
 install.packages("readxl")
 library(readxl)
+library(graphics)
+library(stringr)
 dados <- read_excel("dados/umses_alunos_2018.xlsx")
 
 #Trata os dados, transformando números em strings (1 = "Indefinido", 
@@ -20,3 +22,23 @@ for (opcao in dados$genero) {
 dados$genero
 
 pie(table(dados$genero), col = rainbow(2))
+
+#grafico da faixa etaria
+#pegando dados
+dados <- read_excel("dados/umses_alunos_2018.xlsx") 
+dados2 <- read_excel("dados/dicionario_de_dados.xlsx")
+dados2$X__4[5]
+dados <- as.data.frame(dados) #deixando como data frame
+#dados
+dados2
+dado<- table(dados$idade)
+dado
+
+png(filename="gráficos/faixa_etaria.png", width = 800, height = 500, pointsize = 16)
+
+par(bg = "light blue") #cor do fundo azul claro
+nomes <- c("16 e 20","21 e 25","26 e 30")
+barplot(dado,names.arg = nomes,ylim=c(0,40),xlab = "Idade",ylab = "Frequência",col = rainbow(3),main = "Faixa etária dos questionados")
+dev.off()
+
+
