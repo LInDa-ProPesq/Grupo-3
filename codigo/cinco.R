9b7bb0fcefe55642e11b3d60815017d2f21f5b7f
Vitor de Souza Cruzeiro <vitor@Vitors-MacBook-Pro.local>
2018-11-06 21:18:51 -0200

    bugfixing

--- codigo/cinco.R
+++ codigo/cinco.R
@@ -6,10 +6,11 @@ dados <- read_excel("dados/umses_alunos_2018.xlsx", sheet="dados")
 
 dificuldades <- c(sum(dados$distracao), sum(dados$usoindev), sum(dados$prejintera), sum(dados$bulling), sum(dados$continadeq), 1)
 rotulos <- c("Distracao", "Uso indevido", "Prejuizo da interacao", "Cyberbullying", "Conteudo inadequado", "Outros")
-png(filename="gráficos/cinco.png")
-par(mar = c(8,4,4,2))
+png(filename="gráficos/Secao5_barplot.png", width = 800, height = 600, pointsize = 20)
+par(mar = c(9,4,4,2))
 barplot(dificuldades, col = rainbow(6),
         main = "Dificuldades de uso",
         horiz = F, las = 2,  names.arg = rotulos, cex.names = 0.8, ylim = c(0,60))
 dev.off()
+help(par)
 
